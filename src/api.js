86f521a795481cfdf6e1511c2b550101d9943e39
Karthiknaya <108059989+Karthiknaya@users.noreply.github.com>
2023-12-21 12:52:58 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
-// export const geoApiOptions = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'd9a0e908c2msh948becdd5e88ab2p119f36jsnc3e637562f57',
-// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-// 	}
-// };
-// export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-// export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-// export const WEATHER_API_KEY = "abdc0163065b8c5dc32c607f30ac1698";
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'xxxxxxxxxxxxxxxxxxxxxx',
+		'X-RapidAPI-Host': 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	}
+};
+export const GEO_API_URL = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+export const WEATHER_API_URL = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+export const WEATHER_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxx";
